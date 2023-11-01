b6e01d6cf1b4562c4156bd56b73113bb3d7e569f
Rafael Tury Minatel <rafaelturyminatel@gmail.com>
2023-11-01 17:56:38 -0300

    fix: padding header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import logotipo from '../assets/img/icon-128.png';
 
 export default function Header() {
   return (
-    <div className="w-full flex justify-start items-center bg-[#192b52] gap-10 py-4 px-12">
+    <div className="w-full flex justify-start items-center bg-[#192b52] gap-10 py-2 px-12">
       <img src={logotipo} width={40} alt="Logotipo da extensão GPCT" />
       <h1 className="text-lg font-semibold text-white">Bem vindo à facilidade em suas reuniões!</h1>
     </div>
